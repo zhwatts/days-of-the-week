6c51e87b93b57e41659aee04abd0c82adc5df607
Zach Watts <zach@zachwatts.online>
2022-03-10 21:23:10 -0500

    Refactors validation methods to use Date.Parse and compareFunction sorter

--- index.js
+++ index.js
@@ -1,54 +1,67 @@
 /** @format */
 
-const validateDateStringFormat = (dateString) => {
-	const dateArray = dateString.split("-");
+function isDateFuture(date) {
+	if (date.setHours(0, 0, 0, 0) <= new Date().setHours(0, 0, 0, 0)) {
+		return false;
+	}
 
-	// GENERAL CHECKS
-	if (dateArray.length !== 3) return new Error();
-	if (dateArray.every((item) => isNaN(parseInt(item)))) return new Error();
-	if (dateArray.every((item) => parseInt(item) <= 0)) return new Error();
+	return true;
+}
 
-	// YEAR
-	if (dateArray[0].length < 4) return new Error();
+function isValueDate(date) {
+	let result = false;
 
-	// MONTH
-	if (dateArray[1].length !== 2 || dateArray[1] > 12) return new Error();
-	dateArray[1] = dateArray[1] - 1; // Date.setMonth requires month to be zero indexed
+	switch (true) {
+		case date instanceof Date:
+		case !!Date.parse(date):
+			result = true;
+		default:
+			break;
+	}
 
-	// DAY
-	if (dateArray[2].length !== 2 || dateArray[2] > 31) return new Error();
+	return result;
+}
 
-	return dateArray.map((item) => parseInt(item));
-};
+function renderDateObject(dateValue) {
+	let _formattedDate;
+
+	switch (true) {
+		case dateValue instanceof Date:
+			_formattedDate = dateValue;
+			break;
+		case !!Date.parse(dateValue):
+			const dateArray = dateValue.split("-");
+			dateArray[1] -= 1; // Date() expects month to be zero indexed
+			_formattedDate = new Date(...dateArray); // if 'YYYY-MM-DD' string is passed instead of multi parts, Date() will parse string with GMT timezone offset by default
+			break;
+		default:
+			_formattedDate = new Date();
+	}
+
+	return _formattedDate.setHours(0, 0, 0, 0);
+}
 
 module.exports = function (firstDate, secondDate = new Date()) {
-	const DATE_ONE = new Date();
-	const DATE_TWO = new Date();
-	const DATE_ARRAY = [];
+	let DATE_ONE = new Date();
+	let DATE_TWO = new Date();
+	let ORDERED_DATES = [];
+	let DATE_ARRAY = [];
 
-	/** CHECK THAT PROVIDED ARGUMENTS CAN BE INITIALIZED AS DATE CLASS */
-	if (!(firstDate instanceof Date))
-		try {
-			new Date(...validateDateStringFormat(firstDate)) > secondDate
-				? DATE_TWO.setFullYear(...validateDateStringFormat(firstDate))
-				: DATE_ONE.setFullYear(...validateDateStringFormat(firstDate));
-		} catch (err) {
-			return new Error("First date parameter is not ISO formatted");
-		}
+	if (isValueDate(firstDate)) {
+		DATE_ONE = new Date(renderDateObject(firstDate));
+	}
 
-	if (!(secondDate instanceof Date))
-		try {
-			new Date(...validateDateStringFormat(secondDate)) >
-			new Date(...validateDateStringFormat(firstDate))
-				? DATE_TWO.setFullYear(...validateDateStringFormat(secondDate))
-				: DATE_ONE.setFullYear(...validateDateStringFormat(secondDate));
-		} catch (err) {
-			return new Error("Second date parameter is not ISO formatted");
-		}
+	if (isValueDate(secondDate)) {
+		DATE_TWO = new Date(renderDateObject(secondDate));
+	}
 
-	let cursor = new Date(DATE_ONE);
+	ORDERED_DATES = [DATE_ONE, DATE_TWO].sort(function (a, b) {
+		return a - b;
+	});
 
-	while (cursor <= DATE_TWO) {
+	let cursor = new Date(ORDERED_DATES[0]);
+
+	while (cursor <= ORDERED_DATES[1]) {
 		DATE_ARRAY.push(new Date(cursor));
 
 		cursor.setDate(cursor.getDate() + 1);
